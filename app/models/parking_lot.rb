32c09e5ee1bac9c148e8affe64354f945b3a4c66
wasimhakim <wasimhakim89@gmail.com>
2024-05-03 19:44:57 +0530

    ParkingLot validations

--- app/models/parking_lot.rb
+++ app/models/parking_lot.rb
@@ -4,4 +4,21 @@ class ParkingLot < ApplicationRecord
   # Associations
   # 
   has_many :slots
+
+  # 
+  # Validations
+  #
+  validates :name, presence: true, uniqueness: true
+  validates :total_slots, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :start_hour, :end_hour, presence: true
+  validate :end_hour_after_start_hour
+  
+
+  private
+
+  def end_hour_after_start_hour
+    return unless start_hour && end_hour
+
+    errors.add(:end_hour, "must be after start hour") if end_hour <= start_hour
+  end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -4,4 +4,17 @@ class Slot < ApplicationRecord
   # Associations
   #
   belongs_to :parking_lot
+
+  #
+  # Validations
+  #
+  validate :check_available_slots, on: :create
+
+  private
+
+  def check_available_slots
+    if parking_lot && parking_lot.total_slots <= parking_lot.slots.count
+      errors.add(:base, 'No available slots in the parking lot')
+    end
+  end
 end
