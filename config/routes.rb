bcb3069e37ce4482dda16bed46f8899f5d98b761
wasimhakim <wasimhakim89@gmail.com>
2024-05-03 19:09:26 +0530

    ParkingLot CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :parking_lots
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_02_202131) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_03_133034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parking_lots", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.time "start_hour"
+    t.time "end_hour"
+    t.integer "total_slots"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
