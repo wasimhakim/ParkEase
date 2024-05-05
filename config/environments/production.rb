bfa351d74fd99a7a9d75b8132fe911ee76ddbbab
wasimhakim <wasimhakim89@gmail.com>
2024-05-05 22:11:59 +0530

    Production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -56,6 +57,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ParkEase_production"
 
   config.action_mailer.perform_caching = false
+
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
